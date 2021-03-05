2a26aee6f553174eec6c60de71adfdf4c2000438
Lais Gurgel <laisgurgelf@gmail.com>
2021-03-04 23:47:17 -0300

    Day 5 - NPS calculate

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -30,26 +30,28 @@ class SendMailController {
       })
     }
     
+    
+    const npsPath = resolve(__dirname, "..", "views", "emails", "npsMail.hbs");
+    
+    const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
+      where: { user_id: user.id, value: null},
+      relations: ["user", "survey"],
+    });
+    
     const variables = {
       name: user.name, 
       title: survey.title,
       description: survey.description,
-      id: user.id,
+      id: "",
       link: process.env.URL_MAIL
     };
-    
-    const npsPath = resolve(__dirname, "..", "views", "emails", "npsMail.hbs");
-
-    const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
-      where: [{ id: user.id}, { value:null}],
-      relations: ["user", "survey"],
-    });
 
     if(surveyUserAlreadyExists) {
+      variables.id = surveyUserAlreadyExists.id;
       await SendMailService.execute(email, survey.title, variables, npsPath)
       return response.json(surveyUserAlreadyExists)
     }
-
+    
      //Salvar informações na tabela
     const surveyUser = surveysUsersRepository.create({
       user_id: user.id,
@@ -58,7 +60,7 @@ class SendMailController {
 
     await surveysUsersRepository.save(surveyUser);
     //enviar e-mail para o usuário - instalou biblioteca nodemailer (https://ethereal.email/)
-    
+    variables.id = surveyUser.id;
 
     await SendMailService.execute(email, survey.title, variables, npsPath);
 
